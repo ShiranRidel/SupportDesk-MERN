7d6ce7a6948f2626c5fa361f05a113eabe3297e4
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-07-22 22:20:55 +0300

    Fetch tickets from Backend

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -1,22 +1,36 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/tickets/'
+const API_URL = "/api/tickets/";
 
 // Create new ticket
 const createTicket = async (ticketData, token) => {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  
-    const response = await axios.post(API_URL, ticketData, config)
-  
-    return response.data
-  }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-  const ticketService = {
-    createTicket,
-  }
-  
-  export default ticketService
+  const response = await axios.post(API_URL, ticketData, config);
+
+  return response.data;
+};
+
+// Get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const ticketService = {
+  createTicket,
+  getTickets,
+};
+
+export default ticketService;
