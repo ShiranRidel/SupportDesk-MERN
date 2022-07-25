36da1a903a218f514eaec67a2fac070be42aa095
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-07-25 20:53:36 +0300

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
+
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
